318587b2d63591283291a846cad5a719d37a1ed3
MuRanSea <32297191+MuRanSea@users.noreply.github.com>
2021-08-13 18:25:16 +0800

    Update 冒泡.cs
    修正冒泡排序错误

--- LeetCodeDailyPractice/DailyPractice/排序/冒泡.cs
+++ LeetCodeDailyPractice/DailyPractice/排序/冒泡.cs
@@ -8,18 +8,18 @@ namespace DailyPractice.排序
 {
     class 冒泡
     {
-        public void Sort(int[] nums)
+      public void Sort(int[] nums)
         {
             for (int i = 0; i < nums.Length; i++)
             {
-                for (int j = i+1; j < nums.Length-i; j++)
+                for (int j = 0; j < nums.Length - i - 1; j++)
                 {
-                    if (nums[j] < nums[j - 1])
+                    if (nums[j] > nums[j + 1])
                     {
                         //swap value
-                        var temp = nums[j];
-                        nums[j] = nums[j - 1];
-                        nums[j - 1] = temp;
+                        var temp = nums[j+1];
+                        nums[j+1] = nums[j];
+                        nums[j] = temp;
                     }
                 }
             }
